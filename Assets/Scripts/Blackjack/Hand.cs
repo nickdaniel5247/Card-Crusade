5bf7aeea39a42f9e4119bd8e535d275093467e8d
nickdaniel5247 <146045599+nickdaniel5247@users.noreply.github.com>
2024-10-06 12:23:23 -0700

    Implement hit & double down

--- Assets/Scripts/Blackjack/Hand.cs
+++ Assets/Scripts/Blackjack/Hand.cs
@@ -24,35 +24,42 @@ public class Hand : MonoBehaviour
     }
 
     //Draw card with given sprite and correct transformations
-    private void spawnCard(Sprite card)
+    private void spawnCard(Sprite card, bool rotate = false)
     {
         var gameObject = new GameObject("Card");
         gameObject.transform.SetParent(this.transform);
         gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         gameObject.transform.position = cardObjects.Last().transform.position + new Vector3(0.25f, 0.25f, 0f);
 
+        if (rotate)
+        {
+            gameObject.transform.Rotate(0f, 0f, 90f);
+        }
+
         var spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
         spriteRenderer.sprite = card;
         spriteRenderer.sortingLayerName = "Cards";
+        spriteRenderer.sortingOrder = cardObjects.Count;
+
+        cardObjects.Add(gameObject);
     }
 
     void hit()
     {
-        
+        Card card = deck.takeCard();
+        spawnCard(card.card);
+        cards.Add(card.value);
     }
 
     void split()
     {
-
+        Debug.LogError("Split not implemented yet.");
     }
 
     void doubleDown()
     {
-
-    }
-
-    void Start()
-    {
-        
+        Card card = deck.takeCard();
+        spawnCard(card.card, true);
+        cards.Add(card.value);
     }
 }
